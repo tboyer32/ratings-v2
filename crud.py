08af59065adcc402a84a2af0d1898c06155382ac
Tiffany Boyer <tboyercode@gmail.com>
2022-04-13 18:43:22 -0500

    handle login

--- crud.py
+++ crud.py
@@ -11,6 +11,29 @@ def create_user(email, password):
 
     return user
 
+
+def verify_user(email, password):
+    """Verify user login."""
+
+    return User.query.filter(User.email == email, User.password == password).first()
+
+
+def get_all_users():
+    """Return all users."""
+
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    """Return user by id."""
+
+    return User.query.get(user_id)
+
+def get_user_by_email(email):
+    """Return user by email."""
+
+    return User.query.filter(User.email == email).first()
+
+
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie."""
 
@@ -20,6 +43,18 @@ def create_movie(title, overview, release_date, poster_path):
                   poster_path = poster_path)
 
     return movie
+
+
+def show_all_movies():
+    """Show all movies."""
+
+    return Movie.query.all()
+
+
+def get_movie_by_id(movie_id):
+    """Return movie by id."""
+
+    return Movie.query.get(movie_id)
 
 
 def create_rating(user, movie, score):
--- server.py
+++ server.py
@@ -1,14 +1,98 @@
 """Server for movie ratings app."""
 
-from flask import Flask
+from flask import (Flask, render_template, request, flash, session,
+                   redirect)
+from model import connect_to_db, db
+import crud
+
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 
 
 # Replace this with routes and view functions!
+@app.route('/')
+def view_homepage():
+    """The homepage"""
+
+    return render_template('homepage.html')
+
+
+@app.route('/movies')
+def view_movies():
+    """Show all movies"""
+
+    movies = crud.show_all_movies()
+
+    return render_template('all_movies.html', movies=movies)
+
+
+@app.route('/movies/<movie_id>')
+def view_movie(movie_id):
+    """show movie details"""
+
+    movie = crud.get_movie_by_id(movie_id)
+
+    return render_template('movie_details.html', movie=movie)
+
+
+@app.route('/users')
+def view_users():
+    """Show all users"""
+
+    users = crud.get_all_users()
+
+    return render_template('all_users.html', users=users)
+
+
+@app.route('/users', methods=['POST'])
+def register_user():
+    """Register a user"""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if (crud.get_user_by_email(email)):
+        flash('User already exists')
+    else:
+        user = crud.create_user(email, password)
+        db.session.add(user)
+        db.session.commit()
+        flash('User created successfully')
+
+    return redirect('/')
+
+
+@app.route('/login', methods=["POST"])
+def user_login():
+    """Verify user login"""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    verified_user = crud.verify_user(email, password)
+
+    if verified_user:
+        session['user_id'] = verified_user.user_id
+        flash("Logged in!")
+    else:
+        flash('Login unsuccesful!')
+
+    return redirect('/')
+
+@app.route('/users/<user_id>')
+def view_user(user_id):
+    """Show a user"""
+
+    user = crud.get_user_by_id(user_id)
+
+    return render_template('user_details.html', user=user)
 
 
 if __name__ == "__main__":
     # DebugToolbarExtension(app)
 
+    connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
